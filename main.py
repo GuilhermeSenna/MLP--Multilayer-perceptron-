9c9403b3950d99d58ac20e567f2cf2af779a615e
Diêgo Marcelino <56489559+marcelinodiego@users.noreply.github.com>
2021-11-14 01:14:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,114 +23,99 @@ def casasDecimais(valor):
 
 
 def main():
-
     # Passo 1
     w = GerarPesos(4)
     v = GerarPesos(2)
+    deltaJ = []
 
     # Saída esperada (d) - Porta XOR
     EntradaInicial = [[0, 0], [0, 1], [1, 0], [1, 1]]
     EntradaFinal = []
     variacaoK = []
     deltaJ = []
-    variacaoJ = [0, 0, 0, 0]
+    variacaoJ = []
 
     ResultNeuronio1 = 0
     ResultNeuronio2 = 0
     ResultNeuronio3 = 0
 
     cont = 0
-    for c in range(10):
-        print(f"\n>> {c+1}ª passada:")
-        if c == 0:
-            for i in range(len(EntradaInicial)):
-                vetEntrada = EntradaInicial[i]      # Percorre os possíveis valores [0, 0], [0, 1], [1, 0], [1, 1]
+    while 1:
+        marcador = 0
+        for i in range(len(EntradaInicial)):
+            vetEntrada = EntradaInicial[i]
+            for j in range(len(vetEntrada)):
+                # Passo 3 (uj)
+                ResultNeuronio1 += (w[j] * vetEntrada[j])
+                ResultNeuronio2 += (w[j + 2] * vetEntrada[j])
+
+            print(v[0])
+
+            # Passo 3 (hj)
+            EntradaFinal.append(logistica(ResultNeuronio1))
+            EntradaFinal.append(logistica(ResultNeuronio2))
+
+            # Passo 4 (uk)
+            ResultNeuronio3 = (EntradaFinal[0] * v[0]) + (EntradaFinal[1] * v[1])
+
+            # Passo 4 (yk)
+            ResultObtido = sinal(ResultNeuronio3)
+
+            # XOR
+            if vetEntrada[0] == 0 and vetEntrada[1] == 0:
+                esperado = -1.5
+            elif vetEntrada[0] == 0 and vetEntrada[1] == 1:
+                esperado = 1.5
+            elif vetEntrada[0] == 1 and vetEntrada[1] == 0:
+                esperado = 1.5
+            elif vetEntrada[0] == 1 and vetEntrada[1] == 1:
+                esperado = -1.5
+
+            # Passo 5 (ek)
+            erro = esperado - ResultObtido
+
+            # Passo 6 (en)
+            erroQuadratico = (1 / 2) * (erro ** 2)
+
+            # Passo 7
+            # Gradiente - camada de saida
+            deltaK = erro * ResultObtido * (1 - ResultObtido)
+
+            # Passo 8
+            # Variacao da correcao dos pesos da camada de saída
+            variacaoK.append(casasDecimais(0.5 * deltaK * EntradaFinal[0]))
+            variacaoK.append(casasDecimais(0.5 * deltaK * EntradaFinal[1]))
+
+            # Passo 9
+            for j in range(len(v)):
+                aux = 0
+                for cont in range(len(v)):
+                    aux += (deltaK * v[cont])
+                aux = aux * EntradaFinal[j] * (1 - EntradaFinal[j])
+                deltaJ.append(casasDecimais(aux))
+
+            # Passo 10
+
+            for z in range(len(deltaJ)):
                 for j in range(len(vetEntrada)):
-                    # Passo 3 (uj)
+                    variacaoJ.append(0.5 * deltaJ[z] * vetEntrada[j])
 
-                    ResultNeuronio1 += (w[j] * vetEntrada[j])
-                    ResultNeuronio2 += (w[j + 2] * vetEntrada[j])
-        else:
-            # for k, k_ in enumerate(variacaoK):
-            #     print(f'varicaok {k} - {k_}')
+            # Passo 11
+            for z in range(len(v)):
+                v[z] += variacaoK[z]
+            for z in range(len(w)):
+                w[z] += variacaoJ[z]
 
+            if erro != 0:
+                marcador = 1
+
+            ResultNeuronio1 = 0
+            ResultNeuronio2 = 0
+            EntradaFinal.clear()
             variacaoK.clear()
-
-            # for var, var_ in enumerate(deltaJ):
-            #     print(f'DeltaJ {var} - {var_}')
-
             deltaJ.clear()
-        print(v[0])
-
-        # Passo 3 (hj)
-        EntradaFinal.append(logistica(ResultNeuronio1))
-        EntradaFinal.append(logistica(ResultNeuronio2))
-
-        # EntradaFinal[0] = logistica(ResultNeuronio1)
-        # EntradaFinal[1] = logistica(ResultNeuronio2)
-
-        # Passo 4 (uk)
-        ResultNeuronio3 = (EntradaFinal[0] * v[0]) + (EntradaFinal[1] * v[1])
-
-        # Passo 4 (yk)
-        ResultObtido = sinal(ResultNeuronio3)
-
-        # XOR
-        if vetEntrada[0] == 0 and vetEntrada[1] == 0:
-            esperado = -1
-        elif vetEntrada[0] == 0 and vetEntrada[1] == 1:
-            esperado = 1
-        elif vetEntrada[0] == 1 and vetEntrada[1] == 0:
-            esperado = 1
-        elif vetEntrada[0] == 1 and vetEntrada[1] == 1:
-            esperado = -1
-
-        # Passo 5 (ek)
-        erro = esperado - ResultObtido
-
-        # Passo 6 (en)
-        erroQuadratico = (1/2) * (erro ** 2)
-
-        # Passo 7
-        # Gradiente - camada de saida
-        deltaK = erro * ResultObtido * (1 - ResultObtido)
-
-        # Passo 8
-        # Variacao da correcao dos pesos da camada de saída
-        variacaoK.append(casasDecimais(0.5 * deltaK * EntradaFinal[0]))
-        variacaoK.append(casasDecimais(0.5 * deltaK * EntradaFinal[1]))
-
-        # Passo 9
-        # deltaJ.append(casasDecimais(deltaK * variacaoK[0] * EntradaFinal[0] * (1 - EntradaFinal[0])))
-        # deltaJ.append(casasDecimais(deltaK * variacaoK[0] * EntradaFinal[1] * (1 - EntradaFinal[1])))
-        # deltaJ.append(casasDecimais(deltaK * variacaoK[1] * EntradaFinal[0] * (1 - EntradaFinal[0])))
-        # deltaJ.append(casasDecimais(deltaK * variacaoK[1] * EntradaFinal[1] * (1 - EntradaFinal[1])))
-
-        deltaJ.append(casasDecimais((deltaK * v[0] + deltaK * v[1]) * EntradaFinal[0] * (1 - EntradaFinal[0])))
-        deltaJ.append(casasDecimais((deltaK * v[0] + deltaK * v[1]) * EntradaFinal[1] * (1 - EntradaFinal[1])))
-
-        # Passo 10
-
-        for z in range(len(EntradaInicial)):
-            variacaoJ[0] += (0.5 * deltaJ[0] * EntradaInicial[z][0])
-            variacaoJ[1] += (0.5 * deltaJ[0] * EntradaInicial[z][1])
-            variacaoJ[2] += (0.5 * deltaJ[1] * EntradaInicial[z][0])
-            variacaoJ[3] += (0.5 * deltaJ[1] * EntradaInicial[z][1])
-
-
-        # variacaoJ = 0.5 * deltaJ *
-
-        # Passo 11
-        v[0] += variacaoK[0]
-        v[1] += variacaoK[1]
-
-        w[0] += variacaoJ[0]
-        w[1] += variacaoJ[1]
-        w[2] += variacaoJ[2]
-        w[3] += variacaoJ[3]
-
-
-        EntradaFinal.clear()
+        if marcador == 0:
+            break
 
 
 main()
